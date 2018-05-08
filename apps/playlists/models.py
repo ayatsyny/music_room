321c351055371fa764510178bf9609d8719a2009
ayatsyny <andriy.yatsynyak@gmail.com>
2018-05-08 21:47:33 +0300

    add models app playlists

---
+++ apps/playlists/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from ..songs.models import Songs
+from ..users.models import User
+
+
+class Playlist(models.Model):
+    PUBLIC = 0
+    PRIVATE = 1
+
+    name = models.CharField(_('name'), max_length=50)
+    management = models.BooleanField(_('management'), default=PUBLIC)
+    author = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='playlist')
+    data_created = models.DateTimeField(_('data created'), auto_now=True)
+    vote_time = models.ForeignKey('VoteTime', blank=True, null=True, on_delete=models.DO_NOTHING)
+    songs = models.ManyToManyField(Songs, blank=True, null=True)
+
+    class Meta:
+        verbose_name = _('playlist')
+        verbose_name_plural = _('playlist')
+        ordering = ('name',)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+class Vote(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.DO_NOTHING)
+    song = models.ForeignKey(Songs, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    like = models.BooleanField(_('like'), default=0)
+
+
+class VoteTime(models.Model):
+    begin = models.DateTimeField(_('begin'))
+    final = models.DateTimeField(_('final'))
--- apps/users/models.py
+++ apps/users/models.py
@@ -5,27 +5,17 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    SEX_NONE = 0
-    SEX_MALE = 1
-    SEX_FEMALE = 2
-    SEX_CHOICES = (
-        (SEX_NONE, '-------'),
-        (SEX_MALE, 'men'),
-        (SEX_FEMALE, 'women'),
-    )
     email = models.EmailField(_('email'), unique=True)
-    login = models.CharField(_('login'), max_length=50)
-    first_name = models.CharField(_('name'), max_length=30)
+    login = models.CharField(_('login'), max_length=50, unique=True)
+    first_name = models.CharField(_('name'), max_length=30, blank=True)
     last_name = models.CharField(_('surname'), max_length=30, blank=True)
-    sex = models.SmallIntegerField(_('sex'), choices=SEX_CHOICES, default=SEX_NONE)
-    birth_date = models.DateField(_('birthday'), null=True, blank=True)
-    city = models.CharField(_('city'), max_length=80, blank=True)
     is_staff = models.BooleanField(_('staff status'), default=False,
                                    help_text=_('Designates whether the user can log into this admin ' 'site.'))
     is_active = models.BooleanField(_('active'), default=True,
                                     help_text=_('Designates whether this user should be treated as '
                                                 'active. Unselect this instead of deleting accounts.'))
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+    friends = models.ManyToManyField('self', symmetrical=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['login']
@@ -33,7 +23,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
-        ordering = ('first_name', 'last_name')
+        ordering = ('login',)
 
     def __str__(self):
-        return '{} {}'.format(self.first_name, self.last_name)
+        return '{} {}'.format(self.login, self.email)
