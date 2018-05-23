576d617dee663214585495122576ce42e31fbefa
Andriy Yatsynyak <andriy.yatsynyak@gmail.com>
2018-05-24 02:16:30 +0300

    fix some issue into test result

--- src/musicroom/views.py
+++ src/musicroom/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    account_deezer = None
+    # if request.user.is_authenticated:
+        # account_deezer = AtachService.objects.filter(user=request.user, name='deezer').first()
+    return render(request, 'home.html', {'account_deezer': account_deezer})
+
